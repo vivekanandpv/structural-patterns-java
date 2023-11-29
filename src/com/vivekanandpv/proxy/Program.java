ca27998dd388570ac78dc221284e95543ab01bfa
Vivekanand P V <vivekanandpv@gmail.com>
2023-11-29 12:35:58 +0530

    lazy instantiation of proxy

--- src/com/vivekanandpv/proxy/Program.java
+++ src/com/vivekanandpv/proxy/Program.java
@@ -2,7 +2,7 @@ package com.vivekanandpv.proxy;
 
 public class Program {
     public static void main(String[] args) {
-        Controller proxyController = new ProxyController(new RestController());
+        Controller proxyController = new ProxyController();
 
         proxyController.process("Greetings of the day!");
     }
